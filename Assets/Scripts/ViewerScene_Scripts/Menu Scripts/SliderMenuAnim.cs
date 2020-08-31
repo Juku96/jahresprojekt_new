4fe02657d3789150690a05caf54c8d2b13c9ec8c
NGXN <64264056+NGXN@users.noreply.github.com>
2020-09-01 00:31:15 +0200

    finito

---
+++ Assets/Scripts/ViewerScene_Scripts/Menu Scripts/SliderMenuAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderMenuAnim : MonoBehaviour
+{
+	public GameObject PanelMenu;
+
+    // Start is called before the first frame update
+    public void ShowHideMenu()
+    {
+        if(PanelMenu != null)
+        {
+        	Animator animator = PanelMenu.GetComponent<Animator>();
+        	if(animator != null)
+        	{
+        		bool isOpen = animator.GetBool("show");
+        		animator.SetBool("show", !isOpen);
+        	}
+        }
+    }
+}
